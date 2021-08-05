827310bc8a3f2cb7f720a6010d857241da90402f
spierrot007 <spierrot007@gmail.com>
2021-08-05 18:59:09 +0100

    Add files via upload

---
+++ Debbuging Exercise _ Errors  Corrected.py
@@ -0,0 +1,8 @@
+#debugging errors
+
+def Cal_force(mass, g):
+    #knowing the mass and gravity you calculate the weight:
+    Phy_force = mass * g;
+    print ('%.2f kg of mass gives a weight of %.2f N' %(mass,Phy_force)) 
+  
+  
